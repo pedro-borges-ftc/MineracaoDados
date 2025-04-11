78caf1947dff57af1b9bfba724ae0af1c966acd3
peuborg <peuborg.8@gmail.com>
2025-04-11 14:21:11 -0300

    planejamento aula 6

---
+++ Aula06/6.1.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+
+#5.1 Correlação
+
+# Montando o caminho completo para o arquivo CSV
+caminho = os.path.join("Aula06", "arquivos06", "financiamentos_2024.csv")
+
+# Lendo o arquivo CSV com delimitador ";"
+df = pd.read_csv(caminho, delimiter=";")
+df.info()
+
+# Valores vazios no DataSet
+df.isnull()
+
+# Soma de Valores vazios no DataSet
+df.isnull().sum()
+
+# Removendo dados faltantes do alvo coluna e remove definitivamente
+# df.dropna(subset=["num_ano_financiamento"],inplace=True)
+# df.dropna(subset=["cod_ibge"],inplace=True)
+# df.dropna(subset=["txt_municipio"],inplace=True)
+# df.dropna(subset=["txt_uf"],inplace=True)
+# df.dropna(subset=["vlr_subsidio"],inplace=True)
+df.dropna(subset=["vlr_financiamento"],inplace=True)
+df.info()
+df.isnull()
+df["vlr_financiamento"].isnull().sum()
+
+# Fazer uma lista com os dados faltantes
+dados_faltantes = df.columns[df.isnull().any()].tolist()
+dados_faltantes
+
+# Não vamos tratar os dados do tipo texto, apenas os Int e Float
+dados_faltantes = ['cod_ibge',
+ 'num_ano_financiamento',
+ 'vlr_subsidio']
+
+# Criar um método para substituir os dados faltantes pela média
+for coluna in dados_faltantes:
+    df[coluna].fillna(df[coluna].mean(),inplace=True)
+
+#
+df.info()
---
+++ Aula06/6.2.py
@@ -0,0 +1,51 @@
+from sklearn.impute import SimpleImputer
+import pandas as pd
+import numpy as np
+import os
+
+#5.1 Correlação
+
+# Montando o caminho completo para o arquivo CSV
+caminho = os.path.join("Aula06", "arquivos06", "financiamentos_2024.csv")
+
+# Lendo o arquivo CSV com delimitador ";"
+df = pd.read_csv(caminho, delimiter=";")
+df.info()
+
+# Valores vazios no DataSet
+df.isnull()
+
+# Soma de Valores vazios no DataSet
+df.isnull().sum()
+
+# Removendo dados faltantes do alvo coluna e remove definitivamente
+# df.dropna(subset=["num_ano_financiamento"],inplace=True)
+# df.dropna(subset=["cod_ibge"],inplace=True)
+# df.dropna(subset=["txt_municipio"],inplace=True)
+# df.dropna(subset=["txt_uf"],inplace=True)
+# df.dropna(subset=["vlr_subsidio"],inplace=True)
+df.dropna(subset=["vlr_financiamento"],inplace=True)
+df.info()
+df.isnull()
+df["vlr_financiamento"].isnull().sum()
+
+# Fazer uma lista com os dados faltantes
+dados_faltantes = df.columns[df.isnull().any()].tolist()
+dados_faltantes
+
+# Não vamos tratar os dados do tipo texto, apenas os Int e Float
+dados_faltantes = ['cod_ibge',
+ 'num_ano_financiamento',
+ 'vlr_subsidio']
+
+# Criar um método para impeza de dados usando a média
+imputer = SimpleImputer(strategy='mean')
+
+# scikit-learn é projetada para treinamento, por isso o fit.
+df[dados_faltantes] = imputer.fit_transform(df[dados_faltantes])
+
+#
+df.info()
+
+# Soma de Valores vazios no DataSet
+df.isnull().sum()
